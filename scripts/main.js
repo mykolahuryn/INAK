b4ecb61f4784b12d9f4a9d6c2642c9f79020175e
Mykola Huryn <mukola197022@gmail.com>
2023-11-19 19:23:22 +0100

    add section

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,25 @@ new Swiper('.slider', {
 
   speed: 800,
 });
+
+new Swiper('.slider-testimonials', {
+  breakpoints: {
+    380: {
+      slidesPerView: 1,
+    },
+
+    740: {
+      slidesPerView: 2,
+    },
+
+    1180: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 800,
+  spaceBetween: 100,
+  loop: true,
+});

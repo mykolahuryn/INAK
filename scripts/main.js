79f59a55946212a58b41e1e3a85aee7be517afca
Mykola Huryn <mukola197022@gmail.com>
2024-01-03 13:33:40 +0100

    new commit

--- scripts/main.js
+++ scripts/main.js
@@ -133,10 +133,23 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+$(document).ready(function() {
 
+	//E-mail Ajax Send
+	$(".footer__form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
-
-
-
-
-
+});
